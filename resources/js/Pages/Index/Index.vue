aeda8239ee10d2ff1d1ef765f550006e0059ee08
Rusli Pramono <92093865+Rusli-5200411417@users.noreply.github.com>
2025-12-28 09:51:32 +0700

    laravel-spa Database Manipulation

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+      <div>Index Page</div>
+      <Link href="/hello">Show Page</Link>
+      <div>
+        this message from {{ message }}
+      </div>
+</template>
+
+<script setup>
+  import { Link } from '@inertiajs/vue3';
+  defineProps({ message: String })
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,8 @@
+<template>
+        <div>Show Page</div>
+        <Link href="/">Index Page</Link>
+</template>
+
+<script setup>
+  import { Link } from '@inertiajs/vue3';
+</script>
