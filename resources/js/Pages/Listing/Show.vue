6739587fa2f37d284715d857bae92b47139a4910
Rusli Pramono <92093865+Rusli-5200411417@users.noreply.github.com>
2025-12-30 17:34:25 +0700

    fiture:calculator montly payment

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -18,7 +18,25 @@
                 <template #header>
                     Offer
                 </template>
-                Make an offer
+                <div>
+                    <label class="label">Interest Rate({{interestRate}}%)</label>
+                    <input v-model.number= "interestRate"
+                    type="range" min="0.1" max="30" step="0.1" 
+                    class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
+
+                    <label class="label">Duration ({{duration}} Year)</label>
+                    <input 
+                    v-model.number="duration"
+                    type="range" min="1" max="35" step="1" 
+                    class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
+                </div>
+
+                <div class="text-gray-600 dark:text-gray-300 mt-2">
+                    <div class="text-gray-400">
+                        Your Monthly Payment
+                    </div>
+                    <Price :price="mothlyPayment" class="text-3xl"/>
+                </div>
             </Box>
         </div>
     </div>
@@ -29,7 +47,14 @@ import ListingAddres from '@/Component/ListingAddres.vue';
 import ListingSpace from '@/Component/ListingSpace.vue';
 import Price from '@/Component/Price.vue';
 import Box from '@/Component/UI/Box.vue';
-    defineProps({
+import { useMonthlPayment } from '@/Composables/useMonthlyPayment';
+import { ref } from 'vue'; 
+
+const interestRate = ref(2.5)
+const duration = ref(25) 
+const props =   defineProps({
         listing: Object,
     })
+
+const {mothlyPayment} = useMonthlPayment(props.listing.price,interestRate,duration)
 </script>
