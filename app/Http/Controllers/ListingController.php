2f5e1b9e2156e7066fb5ad0f55572d4df7577606
Rusli Pramono <ruslipramono25@gmail.com>
2026-01-03 10:05:08 +0700

    Paigination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -21,7 +21,7 @@ class ListingController extends Controller
         return inertia(
             'Listing/Index',
             [
-                'listings' => Listing::all()
+                'listings' => Listing::paginate(15)
             ]
          );
     }
---
+++ resources/js/Component/UI/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex gap-1">
+    <template v-for="(link, index) in links" :key="index">
+      
+      <!-- DISABLED LINK -->
+      <span
+        v-if="!link.url"
+        class="py-2 px-4 rounded-md text-gray-400 cursor-not-allowed"
+        v-html="link.label"
+      />
+
+      <!-- ACTIVE / NORMAL LINK -->
+      <Link
+        v-else
+        class="py-2 px-4 rounded-md"
+        :href="link.url"
+        :class="{
+          'bg-indigo-500 dark:bg-indigo-800 text-white': link.active,
+          'text-gray-700 dark:text-gray-300': !link.active
+        }"
+        v-html="link.label"
+      />
+    </template>
+  </div>
+  
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+defineProps({links: Array})
+</script>
